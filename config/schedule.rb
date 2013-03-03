ba22f1a407989f979ad5e2df241b3592e719c9e9
Kunal Shah <me@kunalashah.com>
2013-03-03 13:18:46 -0500

    Do some obviously BS fiddling to time-zone adjust for UTC for now....

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 #
 # set :output, "/path/to/my/cron_log.log"
 #
-every 1.day, at: '3:00 AM' do
+every 1.day, at: '8:00 AM' do
   runner "Resque.enqueue(UpdateQueues)"
 end
 
-every 1.day, at: '1:00 PM' do
+every 1.day, at: '6:00 PM' do
   runner "Resque.enqueue(EmailDispatcher)"
 end
