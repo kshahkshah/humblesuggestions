5cd71e50ed481884ed08c5b39473f0e34bf7336e
Kunal Shah <me@kunalashah.com>
2013-03-03 22:30:20 -0500

    RAM austerity measures

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 2
+worker_processes 1
 
 working_directory File.expand_path('../..', __FILE__)
 
