03a05ddf278c73caa46807680eda60d823cdfed0
Kunal Shah <me@kunalashah.com>
2013-03-13 02:05:36 -0400

    friggin daylight savings messed with me and I do not want to investigate a solution right now

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,6 @@ every 1.day, at: '8:00 AM' do
   runner "Resque.enqueue(UpdateQueues)"
 end
 
-every :weekday, at: '6:00 PM' do
+every :weekday, at: '5:00 PM' do
   runner "Resque.enqueue(EmailDispatcher)"
 end
